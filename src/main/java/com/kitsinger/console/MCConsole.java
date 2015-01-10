d31c8d7a75a87df5cee6d5463bdf64ad91d886b6
SwooshyCueb <swooshycueb@tearmedia.info>
2015-01-09 21:30:10 -0600

    More mod metadata

--- src/main/java/com/kitsinger/console/MCConsole.java
+++ src/main/java/com/kitsinger/console/MCConsole.java
@@ -18,6 +18,7 @@ package com.kitsinger.console;
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *  
  */
+import java.util.Arrays;
 import java.util.Collections;
 
 import net.minecraft.client.Minecraft;
@@ -33,6 +34,7 @@ import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod
@@ -50,11 +52,27 @@ public class MCConsole {
 	public static final String MODID = "MCConsole";
     public static final String MODNAME = "Ingame Console";
     public static final String VERSION = "1.3.6.2-ors0";
+    public static ModMetadata modMetadata;
    
     public static KeyBinding openKey;
     
     @Mod.Instance("MCConsole")
     public static MCConsole instance;
+    
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	modMetadata = event.getModMetadata();
+    	modMetadata.modId		= MODID;
+    	modMetadata.name		= MODNAME;
+    	modMetadata.version		= VERSION;
+    	modMetadata.description	= "An ingame console.";
+    	//modMetadata.url			= "";
+    	//modMetadata.updateUrl	= "";
+    	modMetadata.authorList	= Arrays.asList (new String[] { "simo_415", "Vayner", "tellefma", "SwooshyCueb" });
+    	//modMetadata.credits		= "";
+    	//modMetadata.logoFile    = "/logo.png";
+    }
     
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
@@ -104,6 +122,6 @@ public class MCConsole {
    } */
    
    public String getVersion() {
-      return MCConsole.VERSION;
+      return VERSION;
    }      
 }
